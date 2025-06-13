2674245391fee54c767f214901094c5272678378
DevNadimm <nadimchowdhury87@gmail.com>
2025-06-13 16:20:33 +0600

    update

--- lib/presentation/blocs/task/task_bloc.dart
+++ lib/presentation/blocs/task/task_bloc.dart
@@ -1,12 +1,18 @@
 import 'package:clear_task/core/constants/error_messages.dart';
+import 'package:clear_task/core/utils/widgets/custom_snack_bar.dart';
 import 'package:clear_task/data/models/task_model.dart';
 import 'package:clear_task/data/repositories/task_local_repository.dart';
 import 'package:clear_task/presentation/blocs/task/task_event.dart';
 import 'package:clear_task/presentation/blocs/task/task_state.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:path/path.dart';
 
 class TaskBloc extends Bloc<TaskEvent, TaskState> {
   final TaskLocalRepository taskLocalRepository = TaskLocalRepository();
+
+  // Cache of all tasks
+  List<Task> _cachedTasks = [];
 
   TaskBloc() : super(TaskInitial()) {
     // Fetch all tasks
@@ -14,6 +20,7 @@ class TaskBloc extends Bloc<TaskEvent, TaskState> {
       emit(TaskLoading());
       try {
         final tasks = await taskLocalRepository.fetchTasks();
+        _cachedTasks = tasks;
         emit(TasksLoaded(tasks));
       } catch (e) {
         emit(TaskError(ErrorMessages.fetchFailed));
@@ -22,69 +29,52 @@ class TaskBloc extends Bloc<TaskEvent, TaskState> {
 
     // Create a new task
     on<CreateTask>((event, emit) async {
-      final currentState = state;
       try {
         emit(TaskLoading());
         Task newTask = await taskLocalRepository.createTask(event.task);
-
-        if (currentState is TasksLoaded) {
-          final List<Task> updatedTasks = List.from(currentState.tasks)
-            ..add(newTask);
-          emit(TasksLoaded(updatedTasks));
-        } else {
-          // If no tasks loaded yet, start with new task
-          emit(TasksLoaded([newTask]));
-        }
+        _cachedTasks.add(newTask);
+        emit(TasksLoaded(List.from(_cachedTasks)));
       } catch (e) {
         emit(TaskError(ErrorMessages.createFailed));
       }
     });
 
-    // Edit/update an existing task
+    // Update task
     on<UpdateTask>((event, emit) async {
-      final currentState = state;
       try {
         emit(TaskLoading());
+        final index = _cachedTasks.indexWhere((task) => task.id == event.task.id);
 
-        if (currentState is TasksLoaded) {
-          final List<Task> tasks = List.from(currentState.tasks);
-          final index = tasks.indexWhere((task) => task.id == event.task.id);
-
-          if (index != -1) {
-            final updatedTask = await taskLocalRepository.updateTask(event.task);
-            tasks[index] = updatedTask;
-            emit(TasksLoaded(tasks));
-          } else {
-            emit(TaskError(ErrorMessages.taskNotFound));
-          }
+        if (index != -1) {
+          final updatedTask = await taskLocalRepository.updateTask(event.task);
+          _cachedTasks[index] = updatedTask;
+          emit(TasksLoaded(List.from(_cachedTasks)));
+        } else {
+          emit(TaskError(ErrorMessages.taskNotFound));
         }
       } catch (e) {
         emit(TaskError(ErrorMessages.updateFailed));
       }
     });
 
-    // Delete a task
+    // Delete task
     on<DeleteTask>((event, emit) async {
-      final currentState = state;
       try {
-        emit(TaskLoading());
-
-        if (currentState is TasksLoaded) {
-          final List<Task> tasks = List.from(currentState.tasks);
-          await taskLocalRepository.deleteTask(event.id);
-          tasks.removeWhere((task) => task.id == event.id);
-          emit(TasksLoaded(tasks));
-        }
+        // emit(TaskLoading());
+        await taskLocalRepository.deleteTask(event.id);
+        _cachedTasks.removeWhere((task) => task.id == event.id);
+        emit(TasksLoaded(List.from(_cachedTasks)));
       } catch (e) {
         emit(TaskError(ErrorMessages.deleteFailed));
       }
     });
 
-    // Delete all tasks
+    // Delete all Tasks
     on<DeleteAllTasks>((event, emit) async {
-      emit(TaskLoading());
+      // emit(TaskLoading());
       try {
         await taskLocalRepository.deleteAllTasks();
+        _cachedTasks.clear(); // Clear cache
         emit(TasksLoaded([]));
       } catch (e) {
         emit(TaskError(ErrorMessages.deleteAllFailed));
@@ -93,46 +83,37 @@ class TaskBloc extends Bloc<TaskEvent, TaskState> {
 
     // Toggle task completion
     on<ToggleTaskCompletion>((event, emit) async {
-      final currentState = state;
-
-      if (currentState is TasksLoaded) {
+      if (state is TasksLoaded) {
         try {
           Task updatedTask = event.task;
           updatedTask.isCompleted = !updatedTask.isCompleted;
-
           await taskLocalRepository.updateTask(updatedTask);
 
-          List<Task> updatedTasks = currentState.tasks.map((task) {
-            return task.id == updatedTask.id ? updatedTask : task;
-          }).toList();
+          int index = _cachedTasks.indexWhere((task) => task.id == updatedTask.id);
+          if (index != -1) _cachedTasks[index] = updatedTask;
 
-          emit(TasksLoaded(updatedTasks));
+          emit(TasksLoaded(List.from(_cachedTasks)));
 
-          bool isAllCompleted = updatedTasks.length > 1 && updatedTasks.every((task) => task.isCompleted);
-          if (isAllCompleted) {
-            add(CelebrateAllTasksCompleted());
-          }
+          bool isAllCompleted = _cachedTasks.length > 1 && _cachedTasks.every((task) => task.isCompleted);
+          if (isAllCompleted) add(CelebrateAllTasksCompleted());
         } catch (e) {
-          emit(currentState);
+          emit(state);
         }
       }
     });
 
-    // Celebrate all task completed
+    // Celebrate all tasks completed
     on<CelebrateAllTasksCompleted>((event, emit) {
       emit(CelebrateSuccess());
     });
 
-    // Search tasks
+    // Search Tasks
     on<SearchTasks>((event, emit) async {
       try {
-        List<Task> allTasks = await taskLocalRepository.fetchTasks();
-        String query = event.query.toLowerCase();
-
-        List<Task> filtered = allTasks.where((task) {
+        final query = event.query.toLowerCase();
+        final filtered = _cachedTasks.where((task) {
           return task.title.toLowerCase().contains(query);
         }).toList();
-
         emit(TasksLoaded(filtered));
       } catch (e) {
         emit(TaskError(ErrorMessages.fetchFailed));
