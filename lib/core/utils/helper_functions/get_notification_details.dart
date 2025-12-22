463fc9ca1e41da265309eb0ee268331ae1e35648
DevNadimm <nadimchowdhury87@gmail.com>
2025-12-22 14:16:39 +0600

    update

--- lib/core/utils/helper_functions/get_notification_details.dart
+++ lib/core/utils/helper_functions/get_notification_details.dart
@@ -3,10 +3,19 @@ import 'dart:math';
 class GetNotificationDetails {
   static String getNotificationMessage(String taskName) {
     final messages = [
-      '⏰ Don’t forget: $taskName',
-      '📌 It’s time for: $taskName',
-      '🧠 You planned to: $taskName',
-      '✅ Ready to finish: $taskName?',
+      'Quick task check 👀 — $taskName',
+      'This is pending — $taskName',
+      'One thing left today — $taskName',
+      'Let’s clear this — $taskName',
+      'Focus mode on — $taskName',
+      'Progress waits for no one — $taskName',
+      'Small progress beats none — $taskName',
+      'You’re closer than you think — $taskName',
+      'Just one task — $taskName',
+      'Task in progress — $taskName',
+      'Next up — $taskName',
+      'Pending task — $taskName',
+      'Time to focus on — $taskName',
     ];
 
     return messages[Random().nextInt(messages.length)];
